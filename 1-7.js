cb1bf02478566f7893eb1e22df9a9ec39830d5b2
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 10:34:09 +0900

    Update 1-7.js

--- 1-7.js
+++ 1-7.js
@@ -1,7 +1,7 @@
-function sayhello4() {
+function sayhello19() {
   alert('正解！');
 alert('サッポロビールは、1987年より「箱根駅伝」に協賛。例年、オリジナルCMを放映している。')}
-function sayhello5() {
+function sayhello20() {
   alert('残念！');}
-function sayhello6() {
+function sayhello21() {
   alert('残念！');}
