dcc40fb4c5fdedefe2804f65586de8d997a79ff5
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 10:28:21 +0900

    Create 1-6.js

---
+++ 1-6.js
@@ -0,0 +1,7 @@
+function sayhello1() {
+  alert('残念！');}
+function sayhello2() {
+  alert('正解！');
+alert('「最終兵器彼女」などの作品で知られる高橋しんは、第63回箱根駅伝で山梨学院大の選手として最終10区を走った。')}
+function sayhello3() {
+  alert('残念！');}
