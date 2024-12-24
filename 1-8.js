eaf88641e1c6d71d407a05aec7034eebb3014ba2
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 10:31:36 +0900

    Create 1-8.js

---
+++ 1-8.js
@@ -0,0 +1,7 @@
+function sayhello7() {
+  alert('残念！');}
+function sayhello8() {
+  alert('残念！');}
+function sayhello9() {
+  alert('正解！');
+alert('これまで優勝時の作戦名は、「ハッピー大作戦」「サンキュー大作戦」「ハーモニー大作戦」「負けてたまるか大作戦」などがある。')}
