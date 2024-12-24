394a74aae8e6c2be59a5293acdbd265c6045ad3c
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 10:13:25 +0900

    Update 1-1.js

--- 1-1.js
+++ 1-1.js
@@ -1,6 +1,7 @@
 function sayhello1() {
   alert('残念！');}
 function sayhello2() {
-  alert('正解！');}
+  alert('正解！');
+alert('金栗四三(1891~1983)。箱根駅伝の開催に尽力し、「マラソンの父」とも称される。2019年大河ドラマ「いだてん」で注目を集めた。')}
 function sayhello3() {
   alert('残念！');}
