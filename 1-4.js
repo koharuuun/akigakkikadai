e93a7f41ba06133ff7dfe300335a2180f534de59
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 10:34:56 +0900

    Update 1-4.js

--- 1-4.js
+++ 1-4.js
@@ -1,7 +1,7 @@
-function sayhello1() {
+function sayhello10() {
   alert('残念！');}
-function sayhello2() {
+function sayhello11() {
   alert('正解！');
 alert('早稲田大のルーキーだった2011年の箱根駅伝で1区区間賞。続く2年次も1区で区間賞を獲得した。また、東京オリンピックでは男子マラソンで8位入賞を果たし、箱根から世界へ羽ばたいた。')}
-function sayhello3() {
+function sayhello12() {
   alert('残念！');}
