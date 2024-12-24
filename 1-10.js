94528a54b018e6995361cd4d4cde723f0fcb33d8
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-24 10:37:55 +0900

    Create 1-10.js

---
+++ 1-10.js
@@ -0,0 +1,7 @@
+function sayhello1() {
+  alert('残念！');}
+function sayhello2() {
+  alert('残念！');}
+function sayhello3() {
+  alert('正解！');
+alert('早稲田大に入学し、一年目から2区に抜擢され注目を集めた。現在は、城西大学男子駅伝部の監督を務め、2023年箱根駅伝では三位に導くなど監督としても注目を集めている。')}
