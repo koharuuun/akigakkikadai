0b3a20cd0c4f4211435df0aefd2774f3f07c236c
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-25 16:45:44 +0900

    Update 1-10.js
    Signed-off-by: koharuuun <167832728+koharuuun@users.noreply.github.com>

--- 1-10.js
+++ 1-10.js
@@ -1,7 +1,10 @@
-function sayhello1() {
+function sayhello28() {
   alert('残念！');}
-function sayhello2() {
+function sayhello29() {
   alert('残念！');}
-function sayhello3() {
+function sayhello30() {
   alert('正解！');
 alert('早稲田大に入学し、一年目から2区に抜擢され注目を集めた。現在は、城西大学男子駅伝部の監督を務め、2023年箱根駅伝では三位に導くなど監督としても注目を集めている。')}
+function sayhello31()
+{document.getElementById('hinto').src=
+'./image/kusibe.jpg';}
