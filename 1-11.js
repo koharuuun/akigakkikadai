f63bbd2993673782f2d5087a7afe019f3424bb7a
koharuuun <167832728+koharuuun@users.noreply.github.com>
2024-12-27 08:56:56 +0900

    Update 1-11.js
    Signed-off-by: koharuuun <167832728+koharuuun@users.noreply.github.com>

--- 1-11.js
+++ 1-11.js
@@ -4,5 +4,4 @@
     alert('素晴らしい！これであなたも箱根駅伝博士！！'); }
   else if(( Number('4')<=Number(w1)<Number('8'))){
     alert('すごい！これからも箱根駅伝見てもっと詳しくなろう！'); }
-  else if( Number('0')<=Number(w1)<(Number('4'))){
-      alert('来年からは箱根駅伝を見て詳しくなろう！'); } }
+  else { alert('来年からは箱根駅伝を見て詳しくなろう！'); } }
